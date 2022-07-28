4555cd0c74d23fee3bf6b959d7177e1da41ed945
repentandliveholy <repentandliveholy@gmail.com>
2022-07-29 01:49:55 +0300

    123

--- conf.php
+++ conf.php
@@ -2,9 +2,9 @@
 
 $conf = [
 	//Change to your bot token, check the readme to know how to create and get your bot token.
-	'bot_token' => 'DDDDDDDDD:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	'bot_token' => '5593225545:AAHSNspTaPCqZvQvumMgXikWhakgjdBPoeg',
 	//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
-	'only_trusted' => TRUE,
+	'only_trusted' => FALSE,
 	//Populate the array with the trusted Chat IDs
 	'trusted' => [ 
 		'<chat_id>' 
